cec1ca73322e59c46d8b074b0e4b288f894ced7d
RIYANDO RAJAGUKGUK <riyando15082003@gmail.com>
2025-06-27 23:58:53 +0700

    Delete assets directory

--- assets/js/pembayaran.js
+++
@@ -1,6 +0,0 @@
-function prosesPembayaran() {
-  const metodeTerpilih = document.querySelector('input[name="payment"]:checked').value;
-  const hasil = document.getElementById("hasil-pembayaran");
-
-  hasil.textContent = `Anda memilih metode pembayaran: ${metodeTerpilih}. Proses rental sedang diproses...`;
-}
