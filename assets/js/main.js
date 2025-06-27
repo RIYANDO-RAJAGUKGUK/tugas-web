a3f09fa8c3817d665a26d5bc2c13d9df396c333b
RIYANDO RAJAGUKGUK <riyando15082003@gmail.com>
2025-06-28 00:05:45 +0700

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+function goToProfile() {
+    window.location.href = 'profile.html'; 
+}
+
+function goToLogin() {
+  window.location.href = 'login.html';
+}
+
+function goToCart() {
+  window.location.href = 'cart.html';
+}
